c8fe0431ba3fa062c7498908c48d03fed78c4448
HuynhNe26 <nguyenhoanghuynh.vn@gmail.com>
2025-05-28 12:10:28 +0700

    update

--- app/src/main/java/com/example/unicinema/MainActivity.java
+++ app/src/main/java/com/example/unicinema/MainActivity.java
@@ -1,20 +1,22 @@
 package com.example.unicinema;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.unicinema.databinding.ActivityMainBinding;
+import com.example.unicinema.view.login.LoginActivity;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -34,9 +35,11 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.menu_order, R.id.menu_movie, R.id.menu_theater, R.id.menu_discount, R.id.menu_gift, R.id.menu_contact, R.id.menu_home)
+                R.id.menu_order, R.id.menu_movie, R.id.menu_theater,
+                R.id.menu_discount, R.id.menu_gift, R.id.menu_contact, R.id.menu_home)
                 .setOpenableLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -45,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem action_account = menu.findItem(R.id.action_account);
+        action_account.setOnMenuItemClickListener(item -> {
+            Intent login = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(login);
+            return true;
+            });
         return true;
     }
 
